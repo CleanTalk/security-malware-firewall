e9bdf9935a5e1915ebe30683383c43865bccf522
safronik <Githubishe351@$^>
2021-06-03 12:05:35 +0500

    New: CleantalkSP\Common\Counter class. New: CleantalkSP\SpbctWP\FirewallCounter class. New: CleantalkSP\SpbctWP\SecurityCounter class.

---
+++ lib/CleantalkSP/SpbctWP/Counters/SecurityCounter.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace CleantalkSP\SpbctWP\Counters;
+
+use CleantalkSP\SpbctWP\Helper;
+
+class SecurityCounter extends \CleantalkSP\Common\Counter {
+    
+    static $instance;
+    
+    protected static $default_time_interval__insert = 3600;
+    protected static $default_time_interval__get    = 86400 * 3;
+    
+    protected $option_name = 'spbc_counter__security';
+    
+    protected $structure = array(
+        'pass' => 0,
+        'deny' => 0,
+    );
+    
+    protected function initCounters(){
+        $this->current_interval_name__insert = Helper::time__get_interval_start( static::$default_time_interval__insert );
+        $this->current_interval_name__get    = Helper::time__get_interval_start( static::$default_time_interval__get );
+        $this->data                          = get_option( $this->option_name, array() );
+    }
+    
+    protected function setCounters(){
+        update_option( $this->option_name, $this->data );
+    }
+}
